c8003045db355ac0ee17734f43b3f85655bded41
Zhang-2023 <zhang66.t66@gmail.com>
2024-12-24 22:16:29 -0700

    Made small changes

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,8 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Undergraduate Student",
+          "Data Science Major"
         ],
         autoStart: true,
         loop: true,
