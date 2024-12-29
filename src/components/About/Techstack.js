1b546afaff7f51b8847244ecffe28298fa63c228
Zhang-2023 <zhang66.t66@gmail.com>
2024-12-29 16:30:51 -0700

    FInished About Page, added tooltip and clicking functions

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,71 +1,57 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
+import { Col, Row, OverlayTrigger, Tooltip } from "react-bootstrap";
 import { CgCPlusPlus } from "react-icons/cg";
 import {
   DiJavascript1,
-  DiReact,
-  DiNodejs,
-  DiMongodb,
   DiPython,
   DiGit,
   DiJava,
 } from "react-icons/di";
 import {
-  SiRedis,
-  SiFirebase,
-  SiNextdotjs,
-  SiSolidity,
-  SiPostgresql,
+  SiCsharp,
+  SiD3Dotjs,
+  SiPandas,
+  SiPytorch,
+  SiR,
+  SiScikitlearn,
 } from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
 
 function Techstack() {
+  // Array of technologies with their corresponding icons, names, and links
+  const techStack = [
+    { icon: <CgCPlusPlus />, name: "C++", link: "https://en.wikipedia.org/wiki/C%2B%2B" },
+    { icon: <DiJavascript1 />, name: "JavaScript", link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript" },
+    { icon: <SiCsharp />, name: "C#", link: "https://learn.microsoft.com/en-us/dotnet/csharp/" },
+    { icon: <DiGit />, name: "Git", link: "https://git-scm.com/" },
+    { icon: <DiPython />, name: "Python", link: "https://www.python.org/" },
+    { icon: <SiR />, name: "R", link: "https://www.r-project.org/" },
+    { icon: <DiJava />, name: "Java", link: "https://www.java.com/" },
+    { icon: <SiD3Dotjs />, name: "D3.js", link: "https://d3js.org/" },
+    { icon: <SiPandas />, name: "Pandas", link: "https://pandas.pydata.org/" },
+    { icon: <SiPytorch />, name: "PyTorch", link: "https://pytorch.org/" },
+    { icon: <SiScikitlearn />, name: "Scikit-Learn", link: "https://scikit-learn.org/" },
+  ];
+
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <CgCPlusPlus />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <TbBrandGolang />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSolidity />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiNextdotjs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiFirebase />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiRedis />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostgresql />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJava />
-      </Col>
+      {techStack.map((tech, index) => (
+        <Col xs={4} md={2} className="tech-icons" key={index}>
+          {/* OverlayTrigger adds tooltip functionality */}
+          <OverlayTrigger
+            placement="top"
+            overlay={<Tooltip id={`tooltip-${index}`}>{tech.name}</Tooltip>}
+          >
+            {/* Wrap the icon with a link */}
+            <a href={tech.link} target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none", color: "inherit" }}>
+              <div>{tech.icon}</div>
+            </a>
+          </OverlayTrigger>
+        </Col>
+      ))}
     </Row>
   );
 }
 
 export default Techstack;
+
+
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,31 +1,45 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
+import { Col, Row, OverlayTrigger, Tooltip } from "react-bootstrap";
 import {
   SiVisualstudiocode,
-  SiPostman,
-  SiSlack,
-  SiVercel,
-  SiMacos,
+  SiWindows,
+  SiJupyter,
+  SiRstudio,
+  SiNotion,
+  SiObsidian,
+  SiTodoist,
+  SiGooglecolab
 } from "react-icons/si";
 
 function Toolstack() {
+  // Array of tools with their corresponding icons, names, and links
+  const tools = [
+    { icon: <SiWindows />, name: "Windows", link: "https://www.microsoft.com/windows" },
+    { icon: <SiVisualstudiocode />, name: "Visual Studio Code", link: "https://code.visualstudio.com/" },
+    { icon: <SiJupyter />, name: "Jupyter", link: "https://jupyter.org/" },
+    { icon: <SiRstudio />, name: "RStudio", link: "https://posit.co/products/open-source/rstudio/" },
+    { icon: <SiNotion />, name: "Notion", link: "https://www.notion.so/" },
+    { icon: <SiObsidian />, name: "Obsidian", link: "https://obsidian.md/" },
+    { icon: <SiTodoist />, name: "Todoist", link: "https://todoist.com/" },
+    { icon: <SiGooglecolab />, name: "Google Colab", link: "https://colab.google/" },
+  ];
+
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMacos />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVisualstudiocode />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSlack />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVercel />
-      </Col>
+      {tools.map((tech, index) => (
+        <Col xs={4} md={2} className="tech-icons" key={index}>
+          {/* OverlayTrigger adds tooltip functionality */}
+          <OverlayTrigger
+            placement="top"
+            overlay={<Tooltip id={`tooltip-${index}`}>{tech.name}</Tooltip>}
+          >
+            {/* Wrap the icon with a link */}
+            <a href={tech.link} target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none", color: "inherit" }}>
+              <div>{tech.icon}</div>
+            </a>
+          </OverlayTrigger>
+        </Col>
+      ))}
     </Row>
   );
 }
