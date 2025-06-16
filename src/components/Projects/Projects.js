23dc6b1851d1c29d46b5f4e823b21ee9e00820fb
Zhang-2023 <zhang66.t66@gmail.com>
2025-06-16 14:32:19 -0600

    Added new project and updated RESUME

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,6 +4,7 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import UrbanGrowth from "../../Assets/Projects/UsUrbanGrowthMap.png";
 import D1ChemoGrad from "../../Assets/Projects/d1chemgradsim.png";
+import ChemoVis from "../../Assets/Projects/twodchemokinevis.png"
 
 function Projects() {
   return (
@@ -32,9 +33,19 @@ function Projects() {
             <ProjectCard
               imgPath={D1ChemoGrad}
               isBlog={false}
-              title="1D Tumor Chemokine Gradient Simulation"
+              title="1D Tumor Chemokine Gradient T-Cell Simulation"
               description="A computational simulation coded in R which attempted to model the T-cell movement within a tumor based on the amount of chemokine being produced by 2 different cancer phenotypes."
               ghLink="https://github.com/ZhangHuTony/1D-ChemokineGradientSim/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={ChemoVis}
+              isBlog={false}
+              title="2D Tumor Chemokine Gradient T-Cell Simulation Visualization"
+              description="A project building off of the 1D Tumor Chemokine Gradient T-Cell Simulation Project. This project extends the chemokine diffusion and T-cell movement into 2 dimensions which is then 
+              visualized in the award winning open source visualization software ParaView."
+              ghLink="https://github.com/ZhangHuTony/2D_ChemokineGradientVis"
             />
           </Col>
         </Row>
